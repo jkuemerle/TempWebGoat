6c4aceb1753daeb5bdb645a286e6c25c5c895dd8
jkuemerle <joe@kuemerle.com>
2012-03-16 11:00:30 -0400

    Rough CSRF ready form.

---
+++ WebGoat/Controllers/DiaryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OWASP.WebGoat.NET.Controllers
+{
+    public class DiaryController : Controller
+    {
+        //
+        // GET: /Diary/
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Tell(OWASP.WebGoat.NET.Models.Diary diary)
+        {
+            DatabaseUtilities du = new DatabaseUtilities();
+            du.AddNewPosting("Secret Entry", "Anonymous", diary.Text);
+            return Redirect("~/Default.aspx");
+        }
+
+    }
+}
---
+++ WebGoat/Models/Diary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OWASP.WebGoat.NET.Models
+{
+    public class Diary
+    {
+
+        public string Text { get; set; }
+    }
+}
